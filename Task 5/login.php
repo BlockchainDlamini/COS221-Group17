3fc4589b143dd15286e4dc0053b8d7e43f2dc11f
Tinogwanz <tinogwanz12@gmail.com>
2023-06-07 09:21:20 +0200

    login update

--- Task 5/login.php
+++ Task 5/login.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,19 +9,19 @@
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="icon" media="(prefers-color-scheme: dark)" href="img/favicon-dark/favicon-dark.ico">
     <link rel="icon" media="(prefers-color-scheme: light)" href="img/favicon-light/favicon-light.ico">
-    
+
     <title>Login</title>
 
-    <body>
-        <?php
-            if (isset($_GET['error'])) {
-                $error = $_GET['error'];
-                if ($error == "incorrect username or password") {
-                    echo "<script>alert('Incorrect username or password');</script>";
-                }
-            }
-        ?>
-        <form onsubmit='return validateForm()' action='validate-login.php' method='post'>
+<body>
+    <?php
+    if (isset($_GET['error'])) {
+        $error = $_GET['error'];
+        if ($error == "incorrect username or password") {
+            echo "<script>alert('Incorrect username or password');</script>";
+        }
+    }
+    ?>
+    <form onsubmit='return validateForm()' action='validate-login.php' method='post'>
         <div class="first">
             <div class="second">
                 <div class="top">
@@ -29,7 +30,7 @@
 
                 <div class="inputfield">
                     <input type="text" class="input" placeholder="Username" id="email" name='email' required>
-                    <i class='bx bx-user' ></i>
+                    <i class='bx bx-user'></i>
                 </div>
 
                 <div class="inputfield">
@@ -41,24 +42,22 @@
                     <input type="submit" class="submit" value="Login" id="">
                     <br>
                     <br>
-                    <a href = "signup.php">Don't have an account? Signup here!</a>
+                    <a href="signup.php">Don't have an account? Signup here!</a>
                 </div>
             </div>
-        </div>  
-        </form>
+        </div>
+    </form>
 
     <script>
         function validateForm() {
             var email = document.getElementById("email").value;
             var emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
 
-            if (!emailRegex.test(email) || email === '') { 
+            if (!emailRegex.test(email) || email === '') {
                 alert("Invalid email address.");
                 return false;
             }
-            return true; 
+            return true;
         }
     </script>
 </body>
-
-
