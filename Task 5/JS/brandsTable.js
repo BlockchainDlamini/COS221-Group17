710a8749416aa7d9fa6af0920bda63e471cd05ce
Tinogwanz <tinogwanz12@gmail.com>
2023-06-07 03:56:48 +0200

    delete fixed for brands

--- Task 5/JS/brandsTable.js
+++ Task 5/JS/brandsTable.js
@@ -1,66 +1,66 @@
-    const url = 'https://wheatley.cs.up.ac.za/u22557858/COS221HA/GetWineBottles.php';
+const url = 'https://wheatley.cs.up.ac.za/u22557858/COS221HA/GetWineBottles.php';
 
-    function generateRow(brandID, rowIndex) {
-        var newRow = document.createElement("tr");
-        newRow.innerHTML = '<td></td><td></td><td></td><td></td><td></td><td></td><td><div class="d-flex justify-content-around"><button disabled type="button" onclick="prepareForUpdate(this)" class="btn btn-link btn-floating btn-sm fw-bold wineEdit" data-mdb-toggle="modal" data-mdb-target="#brandsModal" data-mdb-ripple-color="dark" data-brandID="' + brandID + '" data-rowIndex="' + rowIndex + '" title="Edit"><i class="fa-regular fa-pen-to-square fa-xl"></i></button><button disabled type="button" onclick="updateTable(this)" class="btn btn-link btn-floating btn-sm fw-bold wineEdit" data-mdb-toggle="modal" data-mdb-target="#deleteModal" data-mdb-ripple-color="dark" data-brandID="' + brandID + '" data-rowIndex="' + rowIndex + '" title="Delete"><i class="fa-solid fa-trash fa-xl" style="color: #f83a3a;"></i></button></div></td>';
+function generateRow(brandID, rowIndex) {
+    var newRow = document.createElement("tr");
+    newRow.innerHTML = '<td></td><td></td><td></td><td></td><td></td><td></td><td><div class="d-flex justify-content-around"><button disabled type="button" onclick="prepareForUpdate(this)" class="btn btn-link btn-floating btn-sm fw-bold wineEdit" data-mdb-toggle="modal" data-mdb-target="#brandsModal" data-mdb-ripple-color="dark" data-brandID="' + brandID + '" data-rowIndex="' + rowIndex + '" title="Edit"><i class="fa-regular fa-pen-to-square fa-xl"></i></button><button disabled type="button" onclick="" class="btn btn-link btn-floating btn-sm fw-bold wineEdit" data-mdb-toggle="modal" data-mdb-target="#deleteModal" data-mdb-ripple-color="dark" data-brandID="' + brandID + '" data-rowIndex="' + rowIndex + '" title="Delete"><i class="fa-solid fa-trash fa-xl" style="color: #f83a3a;"></i></button></div></td>';
 
-        newRow.classList.add("tableRow");
-        return newRow;
+    newRow.classList.add("tableRow");
+    return newRow;
+}
+
+function prepareForUpdate(updateButton) {
+    const selectedRow = updateButton.closest("tr");
+    const cols = selectedRow.children;
+    const modal = document.getElementsByClassName("modalInputs");
+
+    var m = 0;
+
+    for (var i = 1; i < cols.length - 1; i++) {
+        modal[m++].value = cols[i].innerHTML;
     }
 
-    function prepareForUpdate(updateButton) {
-        const selectedRow = updateButton.closest("tr");
-        const cols = selectedRow.children;
-        const modal = document.getElementsByClassName("modalInputs");
+    const brandID = updateButton.getAttribute("data-brandID");
+    const rowIndex = updateButton.getAttribute("data-rowIndex");
+    sessionStorage.setItem("brandID", brandID);
+    sessionStorage.setItem("rowIndex", rowIndex);
+    console.log("BrandID: " + brandID);
 
-        var m = 0;
+    document.getElementById("btnDone").addEventListener("click", function () {
+        var formData = {};
+        var nameInput = document.getElementById("formName").value;
+        var provinceInput = document.getElementById("formProvince").value;
+        var phoneNumberInput = document.getElementById("formNumber").value;
+        var emailInput = document.getElementById("formEmail").value;
+        var streetAddressInput = document.getElementById("formAddress").value;
+        var postalCodeInput = document.getElementById("formPostalCode").value;
 
-        for (var i = 1; i < cols.length - 1; i++) {
-            modal[m++].value = cols[i].innerHTML;
-        }
+        if (nameInput !== "")
+            formData["name"] = nameInput;
+        if (provinceInput !== "")
+            formData["province"] = provinceInput;
+        if (phoneNumberInput !== "")
+            formData["phoneNumber"] = phoneNumberInput;
+        if (emailInput !== "")
+            formData["email"] = emailInput;
+        if (streetAddressInput !== "")
+            formData["streetAddress"] = streetAddressInput;
+        if (postalCodeInput !== "")
+            formData["postalCode"] = postalCodeInput;
 
-        const brandID = updateButton.getAttribute("data-brandID");
-        const rowIndex = updateButton.getAttribute("data-rowIndex");
-        sessionStorage.setItem("brandID", brandID); 
-        sessionStorage.setItem("rowIndex", rowIndex);
-        console.log("BrandID: " + brandID);
-        
-        document.getElementById("btnDone").addEventListener("click", function() {
-            var formData = {};
-            var nameInput = document.getElementById("formName").value;
-            var provinceInput = document.getElementById("formProvince").value;
-            var phoneNumberInput = document.getElementById("formNumber").value;
-            var emailInput = document.getElementById("formEmail").value;
-            var streetAddressInput = document.getElementById("formAddress").value;
-            var postalCodeInput = document.getElementById("formPostalCode").value;
+        const data = {
+            "Type": "UpdateBrand",
+            "ID": brandID,
+            "UpdateInfo": formData
+        };
 
-            if (nameInput !== "") 
-                formData["name"] = nameInput;
-            if (provinceInput !== "") 
-                formData["province"] = provinceInput;
-            if (phoneNumberInput !== "") 
-                formData["phoneNumber"] = phoneNumberInput;
-            if (emailInput !== "") 
-                formData["email"] = emailInput;
-            if (streetAddressInput !== "") 
-                formData["streetAddress"] = streetAddressInput;
-            if (postalCodeInput !== "") 
-                formData["postalCode"] = postalCodeInput;
-
-            const data = {
-                "Type": "UpdateBrand",
-                "ID": brandID,
-                "UpdateInfo": formData
-            };
-
-            fetch(url, {
-                method: 'POST',
-                headers: {
+        fetch(url, {
+            method: 'POST',
+            headers: {
                 'Content-Type': 'application/json',
                 'Authorization': 'Basic ' + btoa('u22557858:NeimanAris123#')
-                },
-                body: JSON.stringify(data)
-            })
+            },
+            body: JSON.stringify(data)
+        })
             .then(response => response.json())
             .then(result => {
                 if (result.status === "success") {
@@ -68,7 +68,7 @@
                     alert("Update successful!");
                     var table = document.getElementById("tableBody");
                     var row = table.getElementsByTagName("tr")[rowIndex];
-                    var tds = row.querySelectorAll("td"); 
+                    var tds = row.querySelectorAll("td");
                     for (var i = 0; i < tds.length; i++) {
                         if (i === 0)
                             tds[i].textContent = brandID;
@@ -91,78 +91,78 @@
                 console.error(error);
                 alert("Update failed!");
             });
-        });
+    });
+}
+
+function prepareForInsert() {
+    const modal = document.getElementsByClassName("modalInputs");
+    for (var i = 0; i < modal.length; i++) {
+        modal[i].value = "";
     }
 
-    function prepareForInsert() {
-        const modal = document.getElementsByClassName("modalInputs");
-        for (var i = 0; i < modal.length; i++) {
-            modal[i].value = "";
-        }
+    document.getElementsById("btnDone").addEventListener("click", function () {
+        var formData = {};
+        var nameInput = document.getElementById("formName").value;
+        var provinceInput = document.getElementById("formProvince").value;
+        var phoneNumberInput = document.getElementById("formNumber").value;
+        var emailInput = document.getElementById("formEmail").value;
+        var streetAddressInput = document.getElementById("formAddress").value;
+        var postalCodeInput = document.getElementById("formPostalCode").value;
 
-        document.getElementsById("btnDone").addEventListener("click", function() {
-            var formData = {};
-            var nameInput = document.getElementById("formName").value;
-            var provinceInput = document.getElementById("formProvince").value;
-            var phoneNumberInput = document.getElementById("formNumber").value;
-            var emailInput = document.getElementById("formEmail").value;
-            var streetAddressInput = document.getElementById("formAddress").value;
-            var postalCodeInput = document.getElementById("formPostalCode").value;
+        if (nameInput !== "")
+            formData["name"] = nameInput;
+        if (provinceInput !== "")
+            formData["province"] = provinceInput;
+        if (phoneNumberInput !== "")
+            formData["phone_number"] = phoneNumberInput;
+        if (emailInput !== "")
+            formData["email"] = emailInput;
+        if (streetAddressInput !== "")
+            formData["street_address"] = streetAddressInput;
+        if (postalCodeInput !== "")
+            formData["postal_code"] = postalCodeInput;
 
-            if (nameInput !== "") 
-                formData["name"] = nameInput;
-            if (provinceInput !== "") 
-                formData["province"] = provinceInput;
-            if (phoneNumberInput !== "") 
-                formData["phone_number"] = phoneNumberInput;
-            if (emailInput !== "") 
-                formData["email"] = emailInput;
-            if (streetAddressInput !== "") 
-                formData["street_address"] = streetAddressInput;
-            if (postalCodeInput !== "") 
-                formData["postal_code"] = postalCodeInput;
+        const data = {
+            "Type": "AppendBrand",
+            "AppendInfo": formData
+        };
 
-            const data = {
-                "Type": "AppendBrand",
-                "AppendInfo": formData
-            };
-
-            fetch(url, {
-                method: 'POST',
-                headers: {
+        fetch(url, {
+            method: 'POST',
+            headers: {
                 'Content-Type': 'application/json',
                 'Authorization': 'Basic ' + btoa('u22557858:NeimanAris123#')
-                },
-                body: JSON.stringify(data)
-            })
+            },
+            body: JSON.stringify(data)
+        })
             .then(response => response.json())
             .then(result => {
                 if (result.status === "success")
                     alert("Append successful!");
-                else 
+                else
                     alert("Append failed!");
             })
             .catch(error => {
                 console.error(error);
                 alert("Append failed!");
             });
-        });
-    }
+    });
+}
 
-    const data = {
-        "Type": "GetBrand", 
-        "Return": "*", 
-        "Limit": 11
-    };
+const data = {
+    "Type": "GetBrand",
+    "Return": "*",
+    "Limit": 11
+};
 
-    fetch(url, {
-        method: 'POST',
-        headers: {
+fetch(url, {
+    method: 'POST',
+    headers: {
         'Content-Type': 'application/json',
         'Authorization': 'Basic ' + btoa('u22557858:NeimanAris123#')
-        },
-        body: JSON.stringify(data)
-    })
+    },
+    body: JSON.stringify(data)
+})
     .then(response => response.json())
     .then(data => {
         const dataArray = data.data.map(({ brandID, ...brand }) => brand);
@@ -195,32 +195,32 @@
         console.error(error);
     });
 
-    document.getElementById("btnConfirm").addEventListener("click", function() {
-        brandID = sessionStorage.getItem("brandID");
-        const data = {
-            "Type": "DeleteBrand",
-            "ID": brandID
-        };
-        fetch(url, {
-            method: 'POST',
-            headers: {
+document.getElementById("btnConfirm").addEventListener("click", function () {
+    brandID = sessionStorage.getItem("brandID");
+    const data = {
+        "Type": "DeleteBrand",
+        "ID": brandID
+    };
+    fetch(url, {
+        method: 'POST',
+        headers: {
             'Content-Type': 'application/json',
             'Authorization': 'Basic ' + btoa('u22557858:NeimanAris123#')
-            },
-            body: JSON.stringify(data)
-        })
+        },
+        body: JSON.stringify(data)
+    })
         .then(response => response.json())
         .then(data => {
             if (data.status === "success")
                 alert("Delete successful!");
-            else 
+            else
                 alert("Delete failed!");
         })
         .catch(error => {
             console.error(error);
         });
-    });
+});
 
 
 
-  
+
